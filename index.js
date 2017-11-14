ce3dc070b9b33ba4813b1320f96b165a7b0fbff0
Andrew Solomon <andrewsolomon610@gmail.com>
2017-11-14 07:16:45 -0800

    Trial

---
+++ index.js
@@ -0,0 +1,11 @@
+let percent = 50;
+
+var text = prompt("Enter the text you want summarized", "Null");
+alert(EXECUTIVE.administer(text, percent));
+if(confirm("Do you want a better summary?")) {
+    percent += 10;
+    alert(EXECUTIVE.administer(text, percent));
+} else if(confirm("Do you want a smaller summary?")) {
+    percent -= 10;
+    alert(EXECUTIVE.administer(text, percent));
+}
