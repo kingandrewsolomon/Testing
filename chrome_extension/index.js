baa4e9b8051549957c68bc9b48b1b075916e9fac
Andrew Solomon <andrewsolomon610@gmail.com>
2017-11-15 09:33:40 -0800

    Some improvements

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -3,19 +3,33 @@ chrome.runtime.onMessage.addListener(function(message) {
         let percent = 50;
         
         var text = prompt("Enter the text you want summarized", "Null");
-        alert(EXECUTIVE.administer(text, percent));
+        alertMessage(EXECUTIVE.administer(text, percent));
+        
         let better = confirm("Do you want a better summary?");
         while(better) {
             percent += 10;
-            alert(EXECUTIVE.administer(text, percent));
+            alertMessage(EXECUTIVE.administer(text, percent));
             better = confirm("Do you want a better summary?");
         }
 
         let worse = confirm("Do you want a smaller summary?");
         while(worse) {
             percent -= 10;
-            alert(EXECUTIVE.administer(text, percent));
+            alertMessage(EXECUTIVE.administer(text, percent));
             worse = confirm("Do you want a smaller summary?");
         }
     }
 });
+
+function alertMessage(summary) {
+    if (summary.length > 1000){
+        let pages = Math.trunc(summary.length / 1000) + 1;
+        let firstIndex = 0;
+        let lastIndex = 995; //1 / 2 \n
+        for(var i = 1; i < pages + 1; i++) {
+            alert(i + "/" + pages + "\n" + summary.substring(firstIndex, lastIndex));
+            fisrtIndex = lastIndex;
+            lastIndex += 995;
+        }
+    }
+}
