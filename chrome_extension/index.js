772945625bf183707f21c0490a9e5b4c200ac352
Andrew Solomon <andrewsolomon610@gmail.com>
2017-11-15 05:56:42 -0800

    Practically Done and ready to go!

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,18 +1,22 @@
-chrome.runtime.onMessage.addListener(function(message){
-    console.log(message.txt);
-});
-// START = {
-//     summary: function() {
-//         let percent = 50;
+console.log("ready to rock and roll")
+chrome.runtime.onMessage.addListener(function(message) {
+    if (message.txt === "summarize") {
+        let percent = 50;
+        
+        var text = prompt("Enter the text you want summarized", "Null");
+        alert(EXECUTIVE.administer(text, percent));
+        let better = confirm("Do you want a better summary?");
+        while(better) {
+            percent += 10;
+            alert(EXECUTIVE.administer(text, percent));
+            better = confirm("Do you want a better summary?");
+        }
 
-//         var text = prompt("Enter the text you want summarized", "Null");
-//         alert(EXECUTIVE.administer(text, percent));
-//         if(confirm("Do you want a better summary?")) {
-//             percent += 10;
-//             alert(EXECUTIVE.administer(text, percent));
-//         } else if(confirm("Do you want a smaller summary?")) {
-//             percent -= 10;
-//             alert(EXECUTIVE.administer(text, percent));
-//         }
-//     }
-// }
+        let worse = confirm("Do you want a smaller summary?");
+        while(worse) {
+            percent -= 10;
+            alert(EXECUTIVE.administer(text, percent));
+            worse = confirm("Do you want a smaller summary?");
+        }
+    }
+});
